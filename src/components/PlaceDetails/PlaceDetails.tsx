461d3eddaf5ef7df787d2215b0779d2dd1debe1b
hhpr98 <39483775+hhpr98@users.noreply.github.com>
2023-09-27 22:57:00 +0700

    I-41 Added the image list

--- src/components/PlaceDetails/PlaceDetails.tsx
+++ src/components/PlaceDetails/PlaceDetails.tsx
@@ -1,8 +1,40 @@
 import React from "react";
 
+import "./placedetails.css";
+
+// Ref: https://codepen.io/abmin/pen/jZKrze
 const PlaceDetails = (): JSX.Element => {
+  const handleImageClick = (): void => {
+    const targetImage = document.querySelector("#full-image") as HTMLImageElement;
+    const clickedImage = event.target as HTMLImageElement;
+    if (targetImage && clickedImage) {
+      targetImage.setAttribute("src", clickedImage.src);
+      const showElement = document.querySelector("#image-viewer") as HTMLElement;
+      if (showElement) {
+        showElement.style.display = "block"; // Use style.display to show
+      }
+    }
+  };
+
+  const handleCloseImage = (): void => {
+    const imageViewer = document.getElementById("image-viewer");
+    if (imageViewer) {
+      imageViewer.style.display = 'none';
+    }
+  };
+
   return (
-    <div>PlaceDetails</div>
+    <div>
+      <div className="images">
+        <img src="https://www.w3schools.com/howto/img_fjords.jpg" alt="" width="300" height="200" onClick={handleImageClick} />
+        <img src="https://www.w3schools.com/howto/img_fjords.jpg" alt="" width="300" height="200" onClick={handleImageClick} />
+        <img src="https://www.w3schools.com/howto/img_fjords.jpg" alt="" width="300" height="200" onClick={handleImageClick} />
+      </div>
+      <div id="image-viewer">
+        <span className="close" onClick={handleCloseImage}>&times;</span>
+        <img className="modal-content" id="full-image" />
+      </div>
+    </div>
   );
 };
 

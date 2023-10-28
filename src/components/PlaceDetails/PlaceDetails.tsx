7e71aabc853bb27c368aad75884559fed47afb29
hhpr98 <39483775+hhpr98@users.noreply.github.com>
2023-10-29 02:26:49 +0700

    I-41 resolve SVG path

--- src/components/PlaceDetails/PlaceDetails.tsx
+++ src/components/PlaceDetails/PlaceDetails.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 
+import { Plus, Search } from "../../assets/images/index.js";
+
 const PlaceDetails = (): JSX.Element => {
   return (
     <div className="container mx-auto p-4">
@@ -10,9 +12,13 @@ const PlaceDetails = (): JSX.Element => {
           <div className="w-full flex justify-between items-center p-3">
             <h2 className="text-xl font-semibold">Tôi đã đến Đà Nẵng và...</h2>
             <button id="openModalBtn" className="flex items-center bg-black  border border-black text-white font-semibold py-2 px-4 rounded-md transition-colors duration-300">
-              <svg className="w-4 h-4 mr-2 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 6v6m0 0v6m0-6h6m-6 0H6"></path>
-              </svg>
+              <img
+                className="filter brightness-0 invert"
+                src={Plus as string}
+                alt="Plus icon"
+                width={20}
+                height={20}
+              />
               <p className="text-white">Thêm chuyến đi</p>
             </button>
           </div>
@@ -20,9 +26,13 @@ const PlaceDetails = (): JSX.Element => {
             <div className="relative w-full">
               <input type="text" className="w-full  bg-white py-2 pl-10 pr-4 rounded-lg focus:outline-none border-2 border-gray-100 focus:border-black transition-colors duration-300" placeholder="Search..." />
               <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
-                <svg className="w-4 h-4 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-                  <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
-                </svg>
+                <img
+                  className="filter brightness-0 invert"
+                  src={Search as string}
+                  alt="Plus icon"
+                  width={20}
+                  height={20}
+                />
               </div>
             </div>
           </div>
--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,3 +1,7 @@
 declare module '*.png';
 declare module '*.scss';
 declare module '*.js';
+declare module "*.svg" {
+  const value: string;
+  export default value;
+}

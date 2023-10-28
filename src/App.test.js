103b88da88fd682be8de9cad9d7095d95326d736
hhpr98 <39483775+hhpr98@users.noreply.github.com>
2023-10-28 21:17:54 +0700

    I-41 Fix test error with navigate
    The solution can be found at https://stackoverflow.com/questions/70491774/usenavigate-may-be-used-only-in-the-context-of-a-router-component

--- src/App.test.js
+++ src/App.test.js
@@ -1,13 +1,15 @@
 import React from 'react';
 
 import { render, screen } from '@testing-library/react';
+import { BrowserRouter as Router } from "react-router-dom";
+
 import App from './App';
 
 // https://github.com/facebook/jest/issues/9538
 // eslint-disable-next-line no-undef
 test('renders app test', () => {
   // eslint-disable-next-line react/jsx-filename-extension
-  render(<App />);
+  render(<Router><App /></Router>);
   const text = screen.getByText(/(Hành trình của tôi)/i);
   expect(text).toBeInTheDocument();
 });
